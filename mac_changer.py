819ce50422084bbbebbeda516b41f989032ac3bf
gasparsf <gaspar.sf@gmail.com>
2022-01-03 16:06:00 +0000

    added verification of the changed MAC

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -15,6 +16,7 @@ def get_arguments():
         parser.error("[-] Please specify the new MAC address, use --help for more info.")
     return options
 
+
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
@@ -22,5 +24,25 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_search_result:
+        return mac_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
 options = get_arguments()
-change_mac(options.interface,options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC address is " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC successfully changed")
+else:
+    print("[-] MAC has not changed")
